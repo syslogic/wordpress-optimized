29f2f087a558cbd2054914fbf75259edc325ec73
syslogic <martin@codefx.biz>
2013-01-06 00:36:04 +0100

    added info to output.

--- pagoda/wp-install.php
+++ pagoda/wp-install.php
@@ -19,7 +19,7 @@ $c=str_replace('g.','g_c.' ,$d);
 if(!file_exists($b)){
 	if(file_exists($v)){
 		require_once($v);
-		echo "\nRetrieving fresh salts for WordPress v".$wp_version.".";
+		echo "\nWordPress v".$wp_version." - Installer for PagodaBox v1.05";
 	}
 	if(wget('https://api.wordpress.org/secret-key/1.1/salt/',$b)){
 		file_put_contents($d,file_get_contents($a)."\n".file_get_contents($b)."\n".file_get_contents($c));
@@ -43,7 +43,7 @@ function wget($src, $dst){
 	
 	/* cURL stats */
 	$time = $info['total_time']-$info['namelookup_time']-$info['connect_time']-$info['pretransfer_time']-$info['starttransfer_time']-$info['redirect_time'];
-	echo "\nFetched '$src' @ ".abs(round(($info['size_download']*8/$time/1024/1024),2))."MBit/s:\n";
+	echo "Fetched '$src' @ ".abs(round(($info['size_download']*8/$time/1024/1024),2))."MBit/s:\n";
 	return true;
 }
 ?>
