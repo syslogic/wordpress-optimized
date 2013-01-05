53c40e936d83c368996b071347e8576e1bca17e1
syslogic <martin@codefx.biz>
2013-01-06 00:11:12 +0100

    code cleanup.

--- pagoda/wp-install.php
+++ pagoda/wp-install.php
@@ -1,39 +1,45 @@
 <?php
 /*
-	How to apply salt to a dish?
+	WordPress Installer for PagodaBox v1.05
 	
-	written in 2013 by Martin Zeitler
+	Features:
+	
+	- retrieves unique salts on first deployment
+	
+	- the admin is properly set to read-only mode
+	
+	Copyright 2013 by Martin Zeitler
+	
 	http://profiles.wordpress.org/syslogic/
 */
-$x=dirname(__FILE__).'/wp-config.php';
-$a=dirname(__FILE__).'/wp-config_a.php';
-$b=dirname(__FILE__).'/wp-config_b.php';
-$c=dirname(__FILE__).'/wp-config_c.php';
-
+$v=dirname(__FILE__).'/../wp-includes/version.php';
+$d=dirname(__FILE__).'/wp-config.php';
+$a=str_replace('g.','g_a.' ,$d);
+$b=str_replace('g.','g_b.' ,$d);
+$c=str_replace('g.','g_c.' ,$d);
 if(!file_exists($b)){
+	/* retrieve version number */
+	if(file_exists($v)){
+		require_once($v);
+		echo "\nRetrieving fresh salts for WordPress v".$wp_version."\n";
+	}
 	if(wget('https://api.wordpress.org/secret-key/1.1/salt/',$b)){
-		file_put_contents($x,file_get_contents($a)."\n".file_get_contents($b)."\n".file_get_contents($c));
-		foreach(file($b) as $line){
-			preg_match('/^define(\'(\w+)\',\s+\'(.*)\');/', $line, $matches);
-			echo $matches[1].':'.$matches[2];
+		file_put_contents($d,file_get_contents($a)."\n".file_get_contents($b)."\n".file_get_contents($c));
+		unlink($a);unlink($b);unlink($c);
+		echo "\nThe following strings have been configured:\n";
+		foreach(file($b) as $s){
+			preg_match('/^define(\'(\w+)\',\s+\'(.*)\');/', $s, $m);
+			echo $m[1].': '.$m[2];
 		}
 	}
 }
-
-/* helpers */
 function wget($src, $dst){
 	$fp = fopen($dst, 'w');
 	$curl = curl_init();
-	$opt = array(
-		CURLOPT_URL => $src,
-		CURLOPT_HEADER => false,
-		CURLOPT_FILE => $fp
-	);
+	$opt = array(CURLOPT_URL => $src, CURLOPT_HEADER => false, CURLOPT_FILE => $fp);
 	curl_setopt_array($curl, $opt);
 	$rsp = curl_exec($curl);
-	if($rsp===false){
-		die("[cURL] errno:".curl_errno($curl)."\n[cURL] error:".curl_error($curl)."\n");
-	}
+	if($rsp===false){die("[cURL] errno:".curl_errno($curl)."\n[cURL] error:".curl_error($curl)."\n");}
 	$info = curl_getinfo($curl);
 	curl_close($curl);
 	fclose($fp);
@@ -41,13 +47,6 @@ function wget($src, $dst){
 	/* cURL stats */
 	$time = $info['total_time']-$info['namelookup_time']-$info['connect_time']-$info['pretransfer_time']-$info['starttransfer_time']-$info['redirect_time'];
 	echo "Fetched '$src' @ ".abs(round(($info['size_download']*8/$time/1024/1024),2))."MBit/s.\n";
-	
-	/* better make text contents invisible */
-	/* file_put_contents($dst, "<?php\n".file_get_contents($dst)."\n?>"); */
-	echo "Fresh salts have been applied to your WordPress install.\n";
 	return true;
-}
-function format_size($size=0){
-	if($size < 1024){return $size.'b';}elseif($size < 1048576){return round($size/1024,2).'kb';}else{return round($size/1048576,2).'mb';}
 }
 ?>
